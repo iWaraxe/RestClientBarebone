cb9b0bbd023b0591c8f428bc73a8c8b487741c32
iWaraxe <igor.waraxe@gmail.com>
2024-10-17 12:16:41 +0200

    Task 20 zipcodes

--- src/main/java/com/coherentsolutions/restful/OAuth2Client.java
+++ src/main/java/com/coherentsolutions/restful/OAuth2Client.java
@@ -1,17 +1,26 @@
 package com.coherentsolutions.restful;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class OAuth2Client {
+    private static final Logger logger = LoggerFactory.getLogger(OAuth2Client.class);
     private static final String TOKEN_URL = "http://localhost:8080/oauth2/token";
+    private static final String API_BASE_URL = "http://localhost:8080/api";
     private static final String CLIENT_ID = "0oa157tvtugfFXEhU4x7";
     private static final String CLIENT_SECRET = "X7eBCXqlFC7x-mjxG5H91IRv_Bqe1oq7ZwXNA8aq";
 
@@ -59,6 +68,69 @@ public class OAuth2Client {
                 String responseBody = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
                 JSONObject jsonResponse = new JSONObject(responseBody);
                 return jsonResponse.getString("access_token");
+            });
+        }
+    }
+
+    public List<String> getAvailableZipCodes() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(API_BASE_URL + "/zip-codes");
+            httpGet.setHeader("Authorization", "Bearer " + getReadToken());
+
+            return httpClient.execute(httpGet, response -> {
+                String responseBody = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+                JSONArray jsonArray = new JSONArray(responseBody);
+
+                List<String> zipCodes = new ArrayList<>();
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    zipCodes.add(jsonObject.getString("code"));
+                }
+                logger.info("Extracted zip codes: {}", zipCodes);
+                return zipCodes;
+            });
+        }
+    }
+
+    public void addZipCodes(List<String> zipCodes) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(API_BASE_URL + "/zip-codes/expand");
+            httpPost.setHeader("Authorization", "Bearer " + getWriteToken());
+            httpPost.setHeader("Content-Type", "application/json");
+
+            JSONArray jsonArray = new JSONArray(zipCodes);
+            httpPost.setEntity(new StringEntity(jsonArray.toString()));
+
+            httpClient.execute(httpPost, response -> {
+                int statusCode = response.getCode();
+                if (statusCode != 201) {
+                    throw new IOException("Failed to add zip codes. Status code: " + statusCode);
+                }
+                return null;
+            });
+        }
+    }
+
+    public void resetZipCodes(List<String> zipCodes) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(API_BASE_URL + "/zip-codes/reset");
+            httpPost.setHeader("Authorization", "Bearer " + getWriteToken());
+            httpPost.setHeader("Content-Type", "application/json");
+
+            JSONArray jsonArray = new JSONArray(zipCodes);
+            String jsonBody = jsonArray.toString();
+            logger.info("Sending request to reset zip codes: {}", jsonBody);
+            httpPost.setEntity(new StringEntity(jsonBody));
+
+            httpClient.execute(httpPost, response -> {
+                int statusCode = response.getCode();
+                String responseBody = EntityUtils.toString(response.getEntity());
+                logger.info("Reset zip codes response status code: {}", statusCode);
+                logger.info("Reset zip codes response body: {}", responseBody);
+                if (statusCode != 200) {
+                    throw new IOException("Failed to reset zip codes. Status code: " + statusCode + ", Response: " + responseBody);
+                }
+                return null;
             });
         }
     }
---
+++ src/test/java/com/coherentsolutions/restful/OAuth2ClientZipCodeTest.java
@@ -0,0 +1,62 @@
+package com.coherentsolutions.restful;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OAuth2ClientZipCodeTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(OAuth2ClientZipCodeTest.class);
+
+    private OAuth2Client client;
+    private List<String> initialZipCodes = Arrays.asList("10001", "20002", "30003");
+
+    @BeforeEach
+    void setUp() throws IOException {
+        client = OAuth2Client.getInstance();
+        logger.info("Setting up test with initial zip codes: {}", initialZipCodes);
+        client.resetZipCodes(initialZipCodes);
+    }
+
+    @Test
+    void testGetAvailableZipCodes() throws IOException {
+        List<String> zipCodes = client.getAvailableZipCodes();
+        logger.info("Retrieved zip codes: {}", zipCodes);
+        assertNotNull(zipCodes, "Zip codes should not be null");
+        assertFalse(zipCodes.isEmpty(), "Zip codes should not be empty");
+        assertTrue(zipCodes.containsAll(initialZipCodes), "Retrieved zip codes should contain all initial zip codes");
+        assertEquals(initialZipCodes.size(), zipCodes.size(), "Number of retrieved zip codes should match initial count");
+    }
+
+    @Test
+    void testAddZipCodes() throws IOException {
+        List<String> newZipCodes = Arrays.asList("40004", "50005");
+        logger.info("Adding new zip codes: {}", newZipCodes);
+        client.addZipCodes(newZipCodes);
+
+        List<String> updatedZipCodes = client.getAvailableZipCodes();
+        logger.info("Updated zip codes after addition: {}", updatedZipCodes);
+        assertTrue(updatedZipCodes.containsAll(initialZipCodes), "Updated zip codes should contain all initial zip codes");
+        assertTrue(updatedZipCodes.containsAll(newZipCodes), "Updated zip codes should contain all new zip codes");
+        assertEquals(initialZipCodes.size() + newZipCodes.size(), updatedZipCodes.size(), "Total number of zip codes should be sum of initial and new");
+    }
+
+    @Test
+    void testAddDuplicateZipCodes() throws IOException {
+        List<String> newZipCodes = Arrays.asList("40004", "10001", "40004");
+        logger.info("Adding zip codes with duplicates: {}", newZipCodes);
+        client.addZipCodes(newZipCodes);
+
+        List<String> updatedZipCodes = client.getAvailableZipCodes();
+        logger.info("Updated zip codes after adding duplicates: {}", updatedZipCodes);
+        assertTrue(updatedZipCodes.containsAll(initialZipCodes), "Updated zip codes should contain all initial zip codes");
+        assertTrue(updatedZipCodes.contains("40004"), "Updated zip codes should contain the new unique zip code");
+        assertEquals(initialZipCodes.size() + 1, updatedZipCodes.size(), "Total number of zip codes should increase by 1");
+    }
+}
