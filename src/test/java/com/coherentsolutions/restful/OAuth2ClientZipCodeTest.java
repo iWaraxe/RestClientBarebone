6c8470b2f4fef6a0cd4563e64290f6c9e9cbda71
iWaraxe <igor.waraxe@gmail.com>
2024-10-20 22:45:45 +0200

    Task 20 zipcodes

--- src/test/java/com/coherentsolutions/restful/OAuth2ClientZipCodeTest.java
+++ src/test/java/com/coherentsolutions/restful/OAuth2ClientZipCodeTest.java
@@ -1,5 +1,7 @@
 package com.coherentsolutions.restful;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
